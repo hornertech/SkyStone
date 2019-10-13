b88e0c6342f0dcbf71c8a23e0c20141252107c10
Dhruv Gupta <dhruv575@gmail.com>
2019-10-12 20:19:17 -0700

    Fixed double init of Vuforia and removed last year's commented code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/loading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/loading.java
@@ -51,7 +51,6 @@ public class loading extends LinearOpMode {
 
     private static final String VUFORIA_KEY = "AV8zEej/////AAABmVo2vNWmMUkDlkTs5x1GOThRP0cGar67mBpbcCIIz/YtoOvVynNRmJv/0f9Jhr9zYd+f6FtI0tYHqag2teC5GXiKrNM/Jl7FNyNGCvO9zVIrblYF7genK1FVH3X6/kQUrs0vnzd89M0uSAljx0mAcgMEEUiNOUHh2Fd7IOgjlnh9FiB+cJ8bu/3WeKDxnDdqx6JI5BlQ4w7YW+3X2icSRDRlvE4hhuW1VM1BTPQgds7OtHKqUn4Z5w1Wqg/dWiOHxYTww28PVeg3ae4c2l8FUtE65jr2qQdQNc+DMLDgnJ0fUi9Ww28OK/aNrQQnHU97TnUgjLgCTlV7RXpfut5mZWXbWvO6wA6GGkm3fAIQ2IPL";
     private VuforiaLocalizer vuforia = null;
-    private TFObjectDetector tfod;
 
     //Define Position of Camera
     // CAMERA_CHOICE = BACK (rear camera) or FRONT (selfie camera)
@@ -84,8 +83,7 @@ public class loading extends LinearOpMode {
 
 
         int detect_result;
-        initVuforia();
-        initTfod();
+       // initVuforia();
 
         //Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
@@ -190,206 +188,6 @@ public class loading extends LinearOpMode {
 
         targetsSkyStone.deactivate();
 
-
-
-
-//Last year's code:
-/*
-    long start_time = System.currentTimeMillis();
-    long time_taken;
-    if (opModeIsActive()) {
-        if (tfod != null) {
-            tfod.activate();
-        }
-    }
-    //Begin step 1 - Drop from lander
-    Log.i(TAG, "STEP 1: Come Down and Unlatch");
-
-
-    robot.unlatchUsingEncoderPosition(1, 1, 12);        //Unlatching
-    robot.moveRightToPosition(1, 4);                    //Un-hook
-
-
-    robot.moveBackwardForTime(0.25, 350, false);        //Aligning against lander
-    robot.moveForwardForTime(1, 275, true);            //Move forward to Central position
-    time_taken = System.currentTimeMillis() - start_time;
-    Log.i(TAG, "STEP 1: Completed after : " + time_taken + " milli seconds");
-    //End step 1
-
-
-      telemetry.addData("Detection", "Started");
-    telemetry.update();
-    Log.i(TAG, "STEP 2: Detect and Dislodge ");
-
-
-    detect_result = detectOnceTime(robot);
-    if (detect_result == GOLD_MINERAL_FOUND) {
-        Log.i(TAG, "Gold Mineral detected at Center: Knocking off");
-        robot.moveForwardForTime(0.5, 550, true);               //Knock off mineral
-        robot.moveBackwardForTime(0.5, 400, true);              //Move backward to Central Position
-        robot.pause(100);
-        robot.turnWithAngleAnticlockwise(0.5, 80);              //Turn Left
-        time_taken = System.currentTimeMillis() - start_time;
-        Log.i(TAG, "STEP 2: Completed after : " + time_taken + " Milli Seconds");
-        //End step 2
-
-
-          Log.i(TAG, "STEP 3: Drop Team Marker ");
-        //   robot.pause(100);
-        robot.moveForwardAndDropSlide(1, 1350, true);
-        //Moving back
-        //   robot.pause(100);
-        robot.turnWithAngleAnticlockwise(0.5, 45);              //Turn left side
-        while (sensortouch == false) {                                      //Wall Allignment
-
-
-            // send the info back to driver station using telemetry function.
-            // if the digital channel returns true it's HIGH and the button is unpressed.
-            if (digitalTouch.getState() == true) {
-                telemetry.addData("Digital Touch", "Is Not Pressed");
-                robot.moveRightForTime(0.3, 600, false);
-            } else {
-                robot.pause(100);
-                telemetry.addData("Digital Touch", "Is Pressed");
-                robot.moveLeftForTime(0.6, 200, true);
-                robot.moveForwardAndDropSlide(1, 650, true);            //MAKE EDIT HERE IF DOES NOT WORK
-                sensortouch = true;
-            }
-            telemetry.update();
-        }                                                       //Coming out after aligning
-        //Move forward to go and drop teammarker
-        // robot.grabberRotatorMoveTime(1, 2200);                  //Bring grabber down
-        robot.turnWithAngleClockwise(0.5, 10);
-        robot.releaseMineral(10);                               //Drop Teammarker
-        robot.turnWithAngleAnticlockwise(0.5, 10);                            //Drop Teammarker
-
-
-        time_taken = System.currentTimeMillis() - start_time;
-        Log.i(TAG, "STEP 3: Completed after : " + time_taken + " Milli Seconds");
-
-
-
-        Log.i(TAG, "STEP 4: Park At Crater ");
-        robot.moveBackwardForTime(1, 1550, true);
-        robot.moveBackwardForTime(0.5, 400, true);
-        time_taken = System.currentTimeMillis() - start_time;
-        Log.i(TAG, "STEP 4: Completed after : " + time_taken + " Milli Seconds");
-    } else {//Move right 14.5 in.
-        Log.i(TAG, "Silver Mineral Detected at Center: Moving Right");
-        robot.turnWithAngleClockwise(0.5, 8);
-        robot.moveRightForTime(0.5, 800, true);                 //Move Right to look for Mineral at 2nd location
-        detect_result = detectOnceTime(robot);
-        if (detect_result == GOLD_MINERAL_FOUND) {
-            Log.i(TAG, "Gold Mineral detected at Right location: Knocking off");
-            robot.moveForwardForTime(0.5, 475, true);          //Knock off mineral
-            //robot.pause(100);
-            robot.moveBackwardForTime(0.5, 350, true);         //Move back
-            robot.pause(100);
-            robot.turnWithAngleAnticlockwise(0.5, 80);         //Turn Left degress
-            //  robot.moveForwardForTime(1, 400, true);            //Move back to Central Position
-            time_taken = System.currentTimeMillis() - start_time;
-            Log.i(TAG, "STEP 2: Completed after : " + time_taken + " Milli Seconds");
-            //End step 2
-
-
-
-            Log.i(TAG, "STEP 3: Drop Team Marker ");
-            //robot.pause(100);
-            robot.moveForwardAndDropSlide(1, 1800, true);
-            //Moving back
-            //robot.pause(100);
-            robot.turnWithAngleAnticlockwise(0.8, 45);              //Turn left side
-            while (sensortouch == false) {                                      //Wall Allignment
-
-
-                // send the info back to driver station using telemetry function.
-                // if the digital channel returns true it's HIGH and the button is unpressed.
-                if (digitalTouch.getState() == true) {
-                    telemetry.addData("Digital Touch", "Is Not Pressed");
-                    robot.moveRightForTime(0.3, 600, false);
-                } else {
-                    robot.pause(100);
-                    telemetry.addData("Digital Touch", "Is Pressed");
-                    robot.moveLeftForTime(0.6, 200, true);
-                    robot.moveForwardAndDropSlide(1, 700, true);    //MAKE EDIT HERE IF DOES NOT WORK
-                    sensortouch = true;
-                }
-                telemetry.update();
-            }                                                       //Coming out after aligning
-            //Move forward to go and drop teammarker
-            //robot.grabberRotatorMoveTime(1, 2200);                  //Bring grabber down
-            //  robot.turnWithAngleClockwise(0.8, 10);
-            robot.releaseMineral(8);                               //Drop Teammarker
-            //  robot.turnWithAngleAnticlockwise(0.8, 15);
-
-
-            time_taken = System.currentTimeMillis() - start_time;
-            Log.i(TAG, "STEP 3: Completed after : " + time_taken + " Milli Seconds");
-
-
-            Log.i(TAG, "STEP 4: Park At Crater ");
-            robot.moveBackwardForTime(1, 1600, true);
-            time_taken = System.currentTimeMillis() - start_time;
-            Log.i(TAG, "STEP 4: Completed after : " + time_taken + " Milli Seconds");
-        } else { // Knock of Leftmost Mineral
-            Log.i(TAG, "Silver Mineral Detected at Right Location : Knocking of Left Mineral");
-            robot.turnWithAngleClockwise(0.5, 10);             //Slight turn Right to align
-            robot.moveLeftForTime(0.7, 1450, true);            //Move Left to look for Mineral at 3rd location
-            robot.moveForwardForTime(0.5, 575, true);          //Knock off mineral
-            robot.moveBackwardForTime(0.5, 425, true);         //Move back
-            robot.pause(100);
-            robot.turnWithAngleAnticlockwise(0.5, 105);         //Turn Right 90 degress
-            //   robot.pause(100);
-            //   robot.moveBackwardForTime(.65, 500, true);         //Move backward to Central Position
-            time_taken = System.currentTimeMillis() - start_time;
-            Log.i(TAG, "STEP 2: Completed after : " + time_taken + " Milli Seconds");
-            //End step 2
-
-
-            Log.i(TAG, "STEP 3: Drop Team Marker ");
-            //   robot.pause(100);
-            //    robot.moveForwardForTime(1, 1350, true);                //Moving back
-            robot.pause(100);
-            robot.turnWithAngleAnticlockwise(0.5, 45);              //Turn left side
-            while (sensortouch == false) {                                      //Wall Allignment
-
-
-                // send the info back to driver station using telemetry function.
-                // if the digital channel returns true it's HIGH and the button is unpressed.
-                if (digitalTouch.getState() == true) {
-                    telemetry.addData("Digital Touch", "Is Not Pressed");
-                    robot.moveRightForTime(0.3, 600, false);
-                } else {
-                    robot.pause(100);
-                    telemetry.addData("Digital Touch", "Is Pressed");
-                    robot.moveLeftForTime(0.6, 200, true);
-                    robot.moveForwardAndDropSlide(1, 700, true);        //MAKE EDIT HERE IF DOES NOT WORK
-                    sensortouch = true;
-                }
-                telemetry.update();
-            }                                                       //Coming out after aligning & Move forward to go and drop teammarker
-            robot.grabberRotatorMoveTime(1, 600);                  //Bring grabber down
-            robot.turnWithAngleClockwise(0.8, 10);
-            robot.releaseMineral(10);                               //Drop Team Marker
-            if (test) {
-                robot.turnWithAngleAnticlockwise(0.8, 10);
-                time_taken = System.currentTimeMillis() - start_time;
-                Log.i(TAG, "STEP 3: Completed after : " + time_taken + " Milli Seconds");
-
-
-                Log.i(TAG, "STEP 4: Park At Crater ");
-                robot.moveBackwardForTime(1, 1650, true);
-            }
-            time_taken = System.currentTimeMillis() - start_time;
-            Log.i(TAG, "STEP 4: Completed after : " + time_taken + " Milli Seconds");
-        }
-    */
-
-
-        if (tfod != null) {
-            tfod.shutdown();
-        }
-        Log.i(TAG, "================== Autonomous TEST Finished =======================");
     }
 
 
@@ -414,16 +212,5 @@ public class loading extends LinearOpMode {
 
         // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
     }
-
-
-    /**
-     * Initialize the Tensor Flow Object Detection engine.
-     */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
-    }
+    
 }
